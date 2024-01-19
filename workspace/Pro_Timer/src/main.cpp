739bc43ff9ccce4adcbf8d04886799e0f7420001
Harpreet <harpreet.moga@gmail.com>
2024-01-19 18:50:34 +0530

    Updated Project

--- workspace/Pro_Timer/src/main.cpp
+++ workspace/Pro_Timer/src/main.cpp
@@ -1,20 +1,29 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "main.h"
+
+static void protimer_event_dispatcher(protimer_t *const mobj, event_t const *const e);
+static uint8_t process_button_pad_value(uint8_t btn_pad_value);
+static void display_init(void);
+
+/*Main application object */
+static protimer_t protimer;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.begin(9600);
+ 
+   // put your setup code here, to run once:
+  Serial.begin(115200);
+  display_init();
+  Serial.println("Productive timer application");
+  Serial.println("===========================");
+  pinMode(PIN_BUTTON1,INPUT);
+  pinMode(PIN_BUTTON2,INPUT);
+  pinMode(PIN_BUTTON3,INPUT);
+
+  protimer_init(&protimer);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World");
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
