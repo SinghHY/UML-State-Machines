f3115275bbfcd462120f0e583e1cdd424b9ad22b
Harpreet <harpreet.moga@gmail.com>
2024-03-07 09:50:01 +0530

    Added projects

---
+++ workspace/Protimer_SH/test/test_main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <unity.h>
+
+String STR_TO_TEST;
+
+void setUp(void) {
+    // set stuff up here
+    STR_TO_TEST = "Hello, world!";
+}
+
+void tearDown(void) {
+    // clean stuff up here
+    STR_TO_TEST = "";
+}
